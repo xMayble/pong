6cae08190747576b79cbaf41a6ee0eb6b59fa814
Mehbub Rohit <mehburohit@gmail.com>
2024-02-04 23:33:22 -0500

    paddle keystrokes and animation changes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
+
 
 # Create Paddle
 paddle = Turtle()
@@ -24,6 +26,12 @@ def go_down():
 screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_down, "Down")
+
+game_is_on = True 
+while game_is_on:
+    screen.update()
+
+
 
 
 screen.exitonclick()
