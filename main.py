d8ef582769e478d07be1d4ca2180a90609c6b236
Mehbub Rohit <mehburohit@gmail.com>
2024-02-04 23:23:15 -0500

    added keystrokes to paddles

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.title("My Pong Game")
+screen.title("Pong")
+
+# Create Paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,5 @@
+from turtle import Turtle
+
+class Paddle():
+    def __init__(self):
+        self.shape("square")
