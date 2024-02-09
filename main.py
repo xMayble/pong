1d72a17bb2cbf4ccd2209e13fec500f2301c13b1
Mehbub Rohit <mehburohit@gmail.com>
2024-02-08 21:45:23 -0500

    ball collision with paddles

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xor() < -320:
+        ball.bounce_x()
+
 
 
 
