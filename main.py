12bdbc294462820e918fc3e381a6345202e685bf
Mehbub Rohit <mehburohit@gmail.com>
2024-02-08 22:19:16 -0500

    check when ball goes out of bounds

--- main.py
+++ main.py
@@ -33,6 +33,15 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xor() < -320:
         ball.bounce_x()
 
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        
+
 
 
 
